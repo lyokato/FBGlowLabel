938106a335147e416214a5471a2aeee92fe603c3
lyokato <lyo.kato@gmail.com>
2013-11-13 08:58:21 +0900

    adds image on document

--- FBGlowLabelDemo/Pods/Headers/FBGlowLabel/FBGlowLabel.h
+++ FBGlowLabelDemo/Pods/Headers/FBGlowLabel/FBGlowLabel.h
@@ -1,1 +1,9 @@
-../../../../FBGlowLabel/Classes/FBGlowLabel.h
+#import <UIKit/UIKit.h>
+
+@interface FBGlowLabel : UILabel
+@property (nonatomic, assign) CGFloat glowSize;
+@property (nonatomic, retain) UIColor *glowColor;
+@property (nonatomic, assign) CGFloat innerGlowSize;
+@property (nonatomic, retain) UIColor *innerGlowColor;
+@end
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+
 [Document](http://flashback-calendar.com/fbglowlabel)
+
+![Example](http://static.squarespace.com/static/51c2c539e4b032aad7050f2e/t/52821ce0e4b03c032e6c1113/1384258840734/glow.png?format=750w "Example")
 
